93fb0ae5895e853bbc606b79b7b0fde27c75211e
Fornan-II <fornan2@gmail.com>
2018-04-03 15:07:39 -0400

    Moved to Github
    Stopped working off of a USB drive thanks

---
+++ Assets/Scripts/Framework_Extensions/SunDir_Pawn.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunDir_Pawn : Pawn {
+
+    protected bool doFastSpeed = false;
+    public float baseSpeed = 0.0f;
+    public float fastSpeed = 0.0f;
+    protected Vector3 rotation = Vector3.zero;
+
+    // Use this for initialization
+    protected void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    protected void Update()
+    {
+        transform.Rotate(rotation * Time.deltaTime);
+    }
+
+    public virtual void Horizontal(float value)
+    {
+        if (doFastSpeed)
+        {
+            rotation.x = value * fastSpeed;
+        }
+        else
+        {
+            rotation.x = value * baseSpeed;
+        }
+    }
+
+    public virtual void Vertical(float value)
+    {
+        if (doFastSpeed)
+        {
+            rotation.y = value * fastSpeed;
+        }
+        else
+        {
+            rotation.y = value * baseSpeed;
+        }
+    }
+
+    public virtual void Fire1(bool value)
+    {
+        doFastSpeed = value;
+    }
+}
---
+++ Assets/Scripts/Framework_Extensions/SunDir_PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunDir_PlayerController : PlayerController {
+
+    public Pawn pawnToControl;
+
+    // Use this for initialization
+    protected override void Start()
+    {
+        doRequestSpawn = false;
+        base.Start();
+        LogInputStateInfo = false;
+        IsHuman = true;
+        if(pawnToControl)
+        {
+            PossesedPawn = pawnToControl;
+        }
+    }
+
+    // Update is called once per frame
+    protected void Update()
+    {
+
+    }
+
+    public override void Horizontal(float value)
+    {
+        SunDir_Pawn SDP = (SunDir_Pawn)PossesedPawn;
+        if(SDP)
+        {
+            SDP.Horizontal(value);
+        }
+    }
+
+    public override void Vertical(float value)
+    {
+        SunDir_Pawn SDP = (SunDir_Pawn)PossesedPawn;
+        if (SDP)
+        {
+            SDP.Vertical(value);
+        }
+    }
+
+    public override void Fire1(bool value)
+    {
+        SunDir_Pawn SDP = (SunDir_Pawn)PossesedPawn;
+        if (SDP)
+        {
+            SDP.Fire1(value);
+        }
+    }
+}
