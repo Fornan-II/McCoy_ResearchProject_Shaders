2c3c950bcb85c976feb6ee8bbb1d682a5f4b8c9b
Fornan-II <37092360+Fornan-II@users.noreply.github.com>
2018-04-09 15:05:28 -0400

    Started Demo3

---
+++ Assets/Scripts/Image Effects/SotC_ScreenEffect.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SotC_ScreenEffect : MonoBehaviour {
+
+    public Material imageEffectMat;
+
+    //By nature of having this particular function, this script now functions as an image effect script.
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, imageEffectMat);
+    }
+}
